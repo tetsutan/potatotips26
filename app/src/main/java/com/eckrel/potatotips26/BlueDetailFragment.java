d6937a7055242e8f7f379aa3fa23ebd85958e3f5
Tomonori Yano <yano@eckrel.com>
2016-02-17 22:41:06 +0900

    Fix

--- app/src/main/java/com/eckrel/potatotips26/BlueDetailFragment.java
+++ app/src/main/java/com/eckrel/potatotips26/BlueDetailFragment.java
@@ -1,6 +1,25 @@
 package com.eckrel.potatotips26;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class BlueDetailFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blue, container, false);
+
+        TextView textView = (TextView) view.findViewById(R.id.textView);
+        textView.setText("");
+
+        return view;
+
+    }
+
 }
--- app/src/main/java/com/eckrel/potatotips26/RedListFragment.java
+++ app/src/main/java/com/eckrel/potatotips26/RedListFragment.java
@@ -2,12 +2,14 @@ package com.eckrel.potatotips26;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,6 +24,7 @@ public class RedListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_red, container, false);
 
         ListView listView = (ListView) view.findViewById(R.id.listView);
+        listView.setDividerHeight(5);
 
         adapter = new RedAdapter();
         listView.setAdapter(adapter);
@@ -33,24 +36,29 @@ public class RedListFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
-//        String[] items = {"aaaaaaaaa"};
-        String[] items = {"aaaaaaaaa", "bbbbbbbbbbbb"};
-
-        // ここでやっちゃった
-        adapter.setItems(items);
-        adapter.notifyDataSetChanged();
+        String[] items = {"宮本フレデリカ"};
+//
+//        // 間違ってここに書いた
+//        adapter.setItems(items);
+//        adapter.notifyDataSetChanged();
 
         if(items.length == 1) {
 
-            getActivity().getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.content, new BlueDetailFragment())
-                    .commit();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.content, new BlueDetailFragment())
+                            .commit();
+                }
+            },200);
 
         }
         else{
             // ほんとはここ
-//            adapter.setItems(items);
-//            adapter.notifyDataSetChanged();
+            adapter.setItems(items);
+            adapter.notifyDataSetChanged();
         }
 
     }
@@ -80,16 +88,18 @@ public class RedListFragment extends Fragment {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
 
-            TextView textView = (TextView) convertView;
-            if(textView == null) {
-                textView = new TextView(parent.getContext());
-                textView.setTextColor(Color.RED);
-                textView.setTextSize(30);
+            LinearLayout view = (LinearLayout) convertView;
+            if(view == null) {
+                view = (LinearLayout) LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.cell_red, parent, false);
             }
+
+            TextView textView = (TextView) view.findViewById(R.id.textView);
+            textView.setTextSize(30);
 
             textView.setText(getItem(position));
 
-            return textView;
+            return view;
         }
     }
 }
